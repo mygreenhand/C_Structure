2f77b320e96fefba32a3ca9d7f36d88c67d5f713
芝麻 <zm3892@163.com>
2024-01-12 15:58:52 +0800

    增添栈和链栈算法程序

--- stackArrary/stackArrary.cpp
+++ stackArrary/stackArrary.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-#define MAXNUM 500
+const int MAXNUM = 500;
 
 typedef struct stackNode
 {
@@ -14,28 +14,101 @@ typedef struct stackNode
 }Stack;
 
 
-Stack* createStack(){
+void createStack(Stack &s){
 
-    Stack node;
-    node.top = -1;
+    s.top = -1;
+}
 
-    return node;
+//对栈进行判满
+int isFull(Stack s){
+
+    if(s.top >= MAXNUM - 1)
+        return 1;
+    else
+        return 0;
+
+}
+
+//对栈进行判空
+int isEmpty(Stack s){
+
+    if(s.top == -1)
+        return 1;
+    else 
+        return 0;
+
+}
+
+//入栈
+void push(Stack &s,int num){
+
+    if(isFull(s)){
+
+        cout<<"栈已经满了"<<endl;
+
+        return ;
+    }
+
+    //入栈
+    s.dataArr[++s.top] = num; 
+
 }
 
 
-bool insertStack(Stack *node){
+//出栈
+void pop(Stack &s){
 
+    if(isEmpty(s)){
 
+        cout<<"栈已经空了"<<endl;
+        return;
+    }
+
+    //出栈
+    s.dataArr[s.top--];
 
 }
 
+void printStack(Stack s){
+
+    if(isEmpty(s)) 
+        return;
+    
+    for(int i = 0; i <= s.top; i++){
+        cout<<"栈的第"<<i+1<<"数是："<<s.dataArr[i]<<endl;
+    }
+
+}
+
+
+void topStack(Stack s){
+
+    if(isEmpty(s)){
+        cout<<1<<endl;
+        return;
+    }
+
+    cout<<"栈顶数是"<<s.dataArr[s.top]<<endl;
+}
 
 
 int main(){
 
+    Stack s;
 
+    createStack(s);
 
+    for(int i = 1; i <= 10;i++){
 
+        push(s,i);
 
+    }
+
+    printStack(s);
+
+    pop(s);
+    pop(s);
+    printStack(s);
+    
     return 0;
 }
